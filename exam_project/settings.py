68fc6e3702562f22d6606f8be5712cf6de0e632d
Filkin1912 <filkin1912@abv.bg>
2024-09-01 17:34:04 +0300

    Some project

--- exam_project/settings.py
+++ exam_project/settings.py
@@ -55,18 +55,18 @@ WSGI_APPLICATION = 'exam_project.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.sqlite3',python
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'exam_prep',
+        'NAME': 'exam_project',
         'USER': 'postgres',
         'PASSWORD': 'postgrespw',
         'HOST': '127.0.0.1',
-        'PORT': '49153',
+        'PORT': '5432',
     }
 }
 
